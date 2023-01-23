86523b38adcc4bf90ea6b35b42ed86326181ad6f
236662 <236662@edu.p.lodz.pl>
2023-01-23 17:43:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <cmath>
+#include <corecrt_math_defines.h>
 
 #include "Framework.hpp"
 #include "Vector.hpp"
 #include "Matrix.hpp"
 #include "Quaternion.hpp"
 #include "Line.hpp"
+#include "RayTracingRenderEngine.hpp"
 #include "Segment.hpp"
 #include "Sphere.hpp"
 
@@ -17,109 +19,59 @@ Line CreateLineFromTwoPoints(const Vector& point1, const Vector& point2) {
 }
 
 
-int main() {
+float clamp(float x, float mi, float ma)
+{
+	if (x < mi)
+		x = mi;
+	else if (x > ma)
+		x = ma;
 
+	return x;
+}
+
+
+int main() {
+	RayTracingRenderEngine renderer;
 	using std::string;
 	string output("");
-	
-	{ // 1.Znajdź punkt przecięcia prostych.
 
-		Line line1({ 3, 1, 5 }, { -2, 4, 0 });
-		Line line2({ 1, -5, 3 }, { -2, 4, 0 });
+	float yaw = 0.;
+	float pitch = 0.;
+	float radius = 5.;
 
-		if (line1.lineIntersectionCheck(line2))
+	bool loop = true;
+
+	while (loop)
+	{
+		// TO DO
+		// Add input reading here
+
+		yaw = clamp(yaw, -90.0, 90.0);
+
+		if (pitch >= 360.0)
 		{
-			Vector point = line1.getLineIntersectionPoint(line2);
-			output += "1. ";
-			output += point.toString();
-			output += '\n';
+			pitch -= 360.0;
+		}
+		else if (pitch < 0.0)
+		{
+			pitch += 360.0;
 		}
 
+		Vector cameraPosition(0.f, 0.f, 0.f);
 
-		{ // 2. Znajdź kąt między prostymi z zdania {1}.
-			float angle = line1.getAngleBetweenLines(line2) * 180 / Constants::pi<float>;
-			output += "2. ";
-			output += std::to_string(angle);
-			output += '\n';
-		}
+		float pitchRadians = pitch * M_PI / 180.f;
+		float yawRadians = yaw * M_PI / 180.f;
+		cameraPosition.x = radius * std::cos(pitchRadians) * std::cos(yawRadians);
+		cameraPosition.y = radius * std::sin(pitchRadians) * std::cos(yawRadians);
+		cameraPosition.z = radius * std::sin(yawRadians);
 
-	}
+		Vector norm = cameraPosition;
+		norm.normalize();
 
-	{ // 3. Znajdź punkt przecięcia pomiędzy prostymi:
-		Line line1({ 3, -1, 2 }, { -2, 2, -1 });
-		Line line2({ 2, 3, 3 }, { -2, 2, 2 });
+		Vector cameraDirection = norm * -1;
 
-		Vector point = line1.getLineAndPlaneIntersectionPoint(line2);
-		output += "3. ";
-		output += point.toString();
-		output += '\n';
-
-		{ // 4. Znajdź kąt pomiędzy prostą a płaszczyzną z zadania {2}.
-			float angle = line1.getAngleBetweenLineAndPlane(line2) * 180 / Constants::pi<float>;
-			output += "4. ";
-			output += std::to_string(angle);
-			output += '\n';
-		}
-	}
-
-	{ // 5. Znajdź prostą przecięcia płaszczyzn.
-		Line plane1({ 2, -1, 1 }, { 4, 1, 1 });
-		Line plane2({ 4, 3, 1 }, { -4, 1, -1 });
-
-		//Line plane1({ 1, 0, 5 }, { 1, 6, -2 });
-		//Line plane2({ 3, -2, 2 }, { 2, 1, 0 });
-
-		//Line plane1({ 7, 5.5, 2 }, { 1.2, 4, -1.8 });
-		//Line plane2({ 3.4, 3, 5 }, { 2, 1, 1 });
-
-		Line result = plane1.getPlaneIntersection(plane2);
-		output += "5. Directional ";
-		output += result.getDirectional().toString();
-		output += ", Point ";
-		output += result.getPoint().toString();
-		output += '\n';
-
-		{ // 6. Znajdź kąt pomiędzy płaszczyznami z zadania {5}.
-			float angle = plane1.getAngleBetweenLines(plane2) * 180 / Constants::pi<float>;
-			output += "6. ";
-			output += std::to_string(angle);
-			output += '\n';
-		}
-	}
-
-	{ // 7. Znajdź punkt przecięcia dwóch odcinków opisanych punktami:
-		// A { 5, 5, 4 }, A' { 10, 10, 6 }
-		// B { 5, 5, 5 }, B' { 10, 10, 3 } 
-		Segment segmentA({ 5, 5, 4 }, { 10, 10, 6 });
-		Segment segmentB({ 5, 5, 5 }, { 10, 10, 3 });
-
-		if (segmentA.lineBetween.lineIntersectionCheck(segmentB.lineBetween)) {
-			Vector point = segmentA.lineBetween.getLineIntersectionPoint(segmentB.lineBetween);
-			output += "7. ";
-			output += point.toString();
-			output += '\n';
-		}
-
-	}
-
-	{ // 1. AKA 8. Znajdź punkt przecięcia sfery o początku w centrum układu współrzędnych [0, 0, 0] i 
-	  //  promieniu sqrt(26)., oraz prostej przechodzącej przez punkty.
-
-		Sphere sphere({ 0,0,0 }, std::sqrt(26));
-		//Sphere sphere({ 0,0,0 }, 3);
-		//Segment segment({ 3, -1, -2 }, { 5, 3, -4 });
-		//Line line({ 3, -1, -2 }, { 5, 3, -4 });
-		//Line line = Line::FromTwoPoints({ 3, -1, -2 }, { 5, 3, -4 });
-		Vector A { 3, -1, -2 };
-		Vector B { 5, 3, -4 };
-		Line lineBetween = CreateLineFromTwoPoints(A, B);
-
-		Sphere::IntersectionResult result = sphere.getIntersection(lineBetween);
-		output += "8. ";
-		output += result.data()[0].toString();
-		output += result.data()[1].toString();
-		output += '\n';
-
+		renderer.RayCast(cameraPosition, cameraDirection);
+		renderer.Draw();
 	}
 	
 
